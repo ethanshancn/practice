130ae5e7c6b73a0815869e1aa94387e426d2093a
ethan <416225984@qq.com>
2015-09-08 21:45:01 +0800

    常规提交

--- main.c
+++ main.c
@@ -10,18 +10,10 @@
 
 int main(int argc, char **agrv){
 
-    /*int totalNum = 10;
+    int totalNum = 10;
     int arrData[totalNum] ;
     createRadomArray(arrData,totalNum,1,30);
     printIntArray(arrData,totalNum);    //打印数组
-
-    quickSort(arrData,totalNum);
-    printf("After array sort:\n");
-    printIntArray(arrData,totalNum);*/
-
-    int number = gcd(44,68);
-    printf("The number is %d\n",number);
-
 
     return 0;
 }
