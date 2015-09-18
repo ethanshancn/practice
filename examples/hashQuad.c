939ecb128eb1e9cb203d3f3ad77bcc4378042487
Shan <silams@sina.com>
2015-09-18 20:48:34 +0800

    common commit

---
+++ examples/hashQuad.c
@@ -0,0 +1,9 @@
+/**
+ * hashQuad.c : 
+ * @author      Ethan
+ * @date        2015/9/18 20:47
+ * @description :
+ */
+
+
+#include "hashQuad.h"
---
+++ examples/hashQuad.h
@@ -0,0 +1,21 @@
+/**
+ * hashQuad.h : 
+ * @author      Ethan
+ * @date        2015/9/18 20:47
+ * @description :
+ */
+
+
+#ifndef PRACTICE_HASHQUAD_H
+#define PRACTICE_HASHQUAD_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+
+#endif //PRACTICE_HASHQUAD_H
