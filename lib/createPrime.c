5a4dafb7be99430e3f6d11e4942e2a61c5d780a8
ethan <416225984@qq.com>
2015-09-23 23:37:40 +0800

    Common commit

---
+++ lib/createPrime.c
@@ -0,0 +1,9 @@
+/*
+ * createPrime.c : 
+ * author     : ethan
+ * createTime : 15/9/23 下午11:37
+ * description:
+ */
+//
+
+#include "createPrime.h"
---
+++ lib/createPrime.h
@@ -0,0 +1,12 @@
+/*
+ * createPrime.h : 
+ * author     : ethan
+ * createTime : 15/9/23 下午11:37
+ * description:
+ */
+//
+
+#ifndef PRACTICE_CREATEPRIME_H
+#define PRACTICE_CREATEPRIME_H
+
+#endif //PRACTICE_CREATEPRIME_H
