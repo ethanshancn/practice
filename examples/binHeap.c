aa05fe3db29d72f3494c86a04cf32efc59398eba
ethan <416225984@qq.com>
2015-10-01 15:47:58 +0800

    Common commit

--- examples/binHeap.c
+++ examples/binHeap.c
@@ -7,3 +7,35 @@
 //
 
 #include "binHeap.h"
+
+priorityQueue initialize(int maxElement){
+
+}
+
+void destroy(priorityQueue H){
+
+}
+
+void makeEmpty( priorityQueue H){
+
+}
+
+void insert (elementType X, priorityQueue H){
+
+}
+
+elementType deleteMin(priorityQueue H){
+
+}
+
+elementType FindMin(priorityQueue H){
+
+}
+
+int isEmpty(priorityQueue H){
+
+}
+
+int isFull(priorityQueue H){
+
+}
--- examples/binHeap.h
+++ examples/binHeap.h
@@ -9,10 +9,23 @@
 #ifndef PRACTICE_BINHEAP_H
 #define PRACTICE_BINHEAP_H
 
+struct heapStruct;
+typedef struct heapStruct *priorityQueue;
+typedef int elementType;
 
+priorityQueue initialize(int maxElement);
+void destroy(priorityQueue H);
+void makeEmpty( priorityQueue H);
+void insert (elementType X, priorityQueue H);
+elementType deleteMin(priorityQueue H);
+elementType FindMin(priorityQueue H);
+int isEmpty(priorityQueue H);
+int isFull(priorityQueue H);
 
-
-
-
+struct heapStruct{
+    int capacity;
+    int size;
+    elementType *content;
+};
 
 #endif //PRACTICE_BINHEAP_H
